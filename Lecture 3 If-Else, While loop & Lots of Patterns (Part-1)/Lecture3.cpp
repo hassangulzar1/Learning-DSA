743cd883d800d4bcd3f71149d2238bbfd66389ed
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-15 23:28:37 +0500

    lecture 3 done

--- Lecture 3 If-Else, While loop & Lots of Patterns (Part-1)/Lecture3.cpp
+++ Lecture 3 If-Else, While loop & Lots of Patterns (Part-1)/Lecture3.cpp
@@ -69,18 +69,93 @@ int main()
     //     cout << "A is 0" << endl;
     // }
 
-    char a;
-    cin >> a;
-    if (int(a) >= 97 && int(a) <= 122)
+    //! HOME WORK
+    // char a;
+    // cin >> a;
+    // if (int(a) >= 97 && int(a) <= 122)
+    // {
+    //     cout << "This is loweCase" << endl;
+    // }
+    // else if (int(a) >= 65 && int(a) <= 90)
+    // {
+    //     cout << "this is UpperCase" << endl;
+    // }
+    // else if (int(a) >= 48 && int(a) <= 57)
+    // {
+    //     cout << "This is numeric" << endl;
+    // }
+
+    //! While Loop
+    // int i = 1;
+    // int sum = 0;
+    // while (i <= n)
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // while (i <= 5)
+    // {
+    //     sum += i;
+    //     i++;
+    // }
+    // cout << sum << endl;
+
+    // !Practive
+    // int i = 1;
+    // int sumEven = 0;
+    // while (i <= 5)
+    // {
+    //     if (i % 2 == 0)
+    //     {
+    //         sumEven += i;
+    //     }
+    //     i++;
+    // }
+    // cout << sumEven << endl;
+
+    //! PRIME NUMBER
+    // int n;
+    // cin >> n;
+    // int i = 2;
+    // while (i < n)
+    // {
+    //     if (n % i == 0)
+    //     {
+    //         cout << "Your Number is Not Prime" << endl;
+    //     }
+    //     i++;
+    // }
+
+    //! PATTERN 1
+    // int n;
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << "*";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    //! PATTERN 2
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
     {
-        cout << "This is loweCase" << endl;
-    }
-    else if (int(a) >= 65 && int(a) <= 90)
-    {
-        cout << "this is UpperCase" << endl;
-    }
-    else if (int(a) >= 48 && int(a) <= 57)
-    {
-        cout << "This is numeric" << endl;
+        int j = 1;
+        while (j <= n)
+        {
+            cout << i;
+            j++;
+        }
+        cout << endl;
+        i++;
     }
 }
