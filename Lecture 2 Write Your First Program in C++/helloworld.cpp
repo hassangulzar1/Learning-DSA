ffe43866061c30223c7ee913ff0c0f8b36aa4009
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-15 19:14:30 +0500

    first commit

---
+++ Lecture 2 Write Your First Program in C++/helloworld.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //     int b = 123;
+    //     cout << b << endl;
+    //
+    //     char hassan = 'a';
+    //     cout << hassan << endl;
+    //
+    //     bool bl = true;
+    //     cout << bl << endl;
+    //
+    //     float f = 1.2;
+    //     cout << f << endl;
+    //
+    //     double d = 1.23;
+    //     cout << d << endl;
+    //
+    //     int size = sizeof(d);
+    //     cout << "Size of B is: " << size << endl;
+
+    //! Type Casting
+    //     int a = 'a';
+    //     cout << a << endl;
+    //
+    //     char ch = 98;
+    //     cout << ch << endl;
+    //
+    //     char ch1 = 123456;
+    //     cout << ch1 << endl;
+
+    //! unsigne intiger
+    // unsigned int a = -112;
+    // cout << a << endl;
+
+    //! Operators
+    // int a = 2 / 5;
+    // cout << a << endl;
+
+    //     double a = 2.0 / 5;
+    //     cout << a << endl;
+    //
+    //     cout << 2.0 / 5 << endl;
+
+    //! Relational Operators
+
+    //     int a = 2;
+    //     int b = 3;
+    //
+    //     bool first = a == b;
+    //     cout << first << endl;
+
+    // bool second = a > b;
+    // cout << second << endl;
+
+    // int a = 0;
+    // cout << !a << endl;
+}
