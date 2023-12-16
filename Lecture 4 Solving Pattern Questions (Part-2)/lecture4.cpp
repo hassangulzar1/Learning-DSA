507632e32be0ca27d4ff1242ac35d6c5d2b59212
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-16 17:17:18 +0500

    lecture 4 starts

---
+++ Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    //! FIRST PATTERN
+    // int n;
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << j;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    //! SECONT PATTERN
+    // int n;
+    // cin >> n;
+    // int i = 1;
+    // while (i <= n)
+    // {
+    //     int j = 1;
+    //     while (j <= n)
+    //     {
+    //         cout << n - j + 1;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    //! THIRD PATTERN
+}
