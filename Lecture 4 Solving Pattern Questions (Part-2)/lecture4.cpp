d6ef766f6fd26510b43d54bfd5c9fdf6d24c759b
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-16 17:27:19 +0500

    third pattren done

--- Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
+++ Lecture 4 Solving Pattern Questions (Part-2)/lecture4.cpp
@@ -37,4 +37,21 @@ int main()
     // }
 
     //! THIRD PATTERN
+    //     int n;
+    //     cin >> n;
+    //     int i = 1;
+    //     int count = 1;
+    //
+    //     while (i <= n)
+    //     {
+    //         int j = 1;
+    //         while (j <= n)
+    //         {
+    //             cout << count;
+    //             j++;
+    //             count++;
+    //         }
+    //         cout << endl;
+    //         i++;
+    //     }
 }
