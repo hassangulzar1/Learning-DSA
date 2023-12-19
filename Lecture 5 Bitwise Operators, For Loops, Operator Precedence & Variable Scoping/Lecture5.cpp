5f34aee9c29bf5cbbe4db1291946209a999e02a4
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-19 22:36:06 +0500

    working

--- Lecture 5 Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/Lecture5.cpp
+++ Lecture 5 Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/Lecture5.cpp
@@ -4,10 +4,47 @@ using namespace std;
 int main()
 {
     //! Bitwise operator
-    int a = 4;
-    int b = 6;
-    cout << "a&b: " << (a & b) << endl;
-    cout << "a|b: " << (a | b) << endl;
-    cout << "~a: " << ~a << endl;
-    cout << "a^b: " << (a ^ b) << endl;
+    // int a = 4;
+    // int b = 6;
+    // cout << "a&b: " << (a & b) << endl;
+    // cout << "a|b: " << (a | b) << endl;
+    // cout << "~a: " << ~a << endl;
+    // cout << "a^b: " << (a ^ b) << endl;
+
+    //! left shift operator / Right Shift operator
+    // cout << (17 >> 1) << endl;
+    // cout << (17 >> 2) << endl;
+    // cout << (19 << 1) << endl;
+    // cout << (21 << 2) << endl;
+
+    //! Inc/ Dex operator
+
+    // int i = 7;
+    // cout << (++i);
+    // cout << (i++);
+    // cout << (i--);
+    // cout << (--i);
+
+    //! for loop
+
+    // int n;
+    // cin >> n;
+    // int i = 1;
+    // for (;;)
+    // {
+    //     if (i <= n)
+    //     {
+    //         cout << i << endl;
+    //     }
+    //     else
+    //     {
+    //         break;
+    //     }
+    //     i++;
+    // }
+    //
+    for (int a = 0, b = 1; a >= 0 && b >= 1; a--, b--)
+    {
+        cout << a << " " << b << endl;
+    }
 }
