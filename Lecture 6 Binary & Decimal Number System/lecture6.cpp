b5bbb4f2f08edd887ae666378e5d02552ca1e194
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-24 23:20:47 +0500

    lecture 6 half done

--- Lecture 6 Binary & Decimal Number System/lecture6.cpp
+++ Lecture 6 Binary & Decimal Number System/lecture6.cpp
@@ -4,19 +4,43 @@ using namespace std;
 
 int main()
 {
+
+    //! decimal to binary +ve numbers
+    // int n;
+    // cin >> n;
+    //     int answer = 0;
+    //     int i = 0;
+    //     while (n != 0)
+    //     {
+    //         int bit = n & 1;
+    //         answer = (bit * pow(10, i)) + answer;
+    //
+    //         n = n >> 1;
+    //         i++;
+    //     }
+    //
+    //     cout << answer << endl;
+
+    //! decimal to binary -ve numbers
+
     int n;
     cin >> n;
 
-    int answer = 0;
-    int i = 0;
     while (n != 0)
     {
-        int bit = n & 1;
-        answer = (bit * pow(10, i)) + answer;
-
+        cout << n % 10 << endl;
         n = n >> 1;
-        i++;
     }
 
-    cout << answer << endl;
+    // int ans = 0;
+    // int i = 0;
+    //     while (n != 0)
+    //     {
+    //         int bit = n & 1;
+    //         ans = (bit * pow(10, i) + ans);
+    //
+    //         n = n >> 1;
+    //         i++;
+    //     }
+    //     cout << ans << endl;
 }
