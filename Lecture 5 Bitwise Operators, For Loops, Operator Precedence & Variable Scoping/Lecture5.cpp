6954fbd593a513981c49a90943d31e09f00d41cc
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-24 14:45:03 +0500

    prme number in c++

--- Lecture 5 Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/Lecture5.cpp
+++ Lecture 5 Bitwise Operators, For Loops, Operator Precedence & Variable Scoping/Lecture5.cpp
@@ -43,8 +43,48 @@ int main()
     //     i++;
     // }
     //
-    for (int a = 0, b = 1; a >= 0 && b >= 1; a--, b--)
-    {
-        cout << a << " " << b << endl;
-    }
+    // for (int a = 0, b = 1; a >= 0 && b >= 1; a--, b--)
+    // {
+    //     cout << a << " " << b << endl;
+    // }
+
+    // int sum = 0;
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     sum += i;
+    // }
+    // cout << sum << endl;
+
+    // int n = 5;
+    // int a = 0;
+    // int b = 1;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     int sum = a + b;
+    //     cout << sum << endl;
+    //     a = b;
+    //     b = sum;
+    // }
+
+    //! Prime Number
+    //     int n = 7;
+    //     bool val = 1;
+    //
+    //     for (int i = 2; i < n; i++)
+    //     {
+    //         if (n % i == 0)
+    //         {
+    //             val = 0;
+    //             break;
+    //         }
+    //     }
+    //
+    //     if (val == 0)
+    //     {
+    //         cout << "Not A Prime Number" << endl;
+    //     }
+    //     else
+    //     {
+    //         cout << "Is a prime number" << endl;
+    //     }
 }
