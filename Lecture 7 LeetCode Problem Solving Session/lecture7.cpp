ef406dc36c9ccc1970238672927c085534780480
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-25 16:13:46 +0500

    lecture 7 done

--- Lecture 7 LeetCode Problem Solving Session/lecture7.cpp
+++ Lecture 7 LeetCode Problem Solving Session/lecture7.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
@@ -36,7 +37,14 @@ int main()
     //     int ans = (~n) & mask;
     //     return ans;
 
-    int n;
-    cin >> n;
-    cout << (n >> 1) << endl;
+    // THIRD PROBLEM
+    //  for (int i = 0; i <= 30; i++)
+    //  {
+    //      int ans = pow(2, i);
+    //      if (ans == n)
+    //      {
+    //          return true;
+    //      }
+    //  }
+    //  return false;
 }
