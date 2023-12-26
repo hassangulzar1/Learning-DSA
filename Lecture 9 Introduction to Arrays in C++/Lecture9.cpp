647975c32c61aedb46f41f367abbe5eb4b62fc3e
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-26 14:13:08 +0500

    starts lecture 9

---
+++ Lecture 9 Introduction to Arrays in C++/Lecture9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //! entire array initilize with same value
+    // int dost[1000] = {-1};
+    // fill_n(dost, 1000, 3);
+    // for (int i = 0; i < 1000; i++)
+    // {
+    //     cout << dost[i] << endl;
+    // }
+
+    return 0;
+}
