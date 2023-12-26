a01beeafbb96e7c41aeffc3e060742d5f04a4a67
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-26 18:57:31 +0500

    almost done

--- Lecture 9 Introduction to Arrays in C++/Lecture9.cpp
+++ Lecture 9 Introduction to Arrays in C++/Lecture9.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    //! entire array initilize with same value
-    // int dost[1000] = {-1};
-    // fill_n(dost, 1000, 3);
-    // for (int i = 0; i < 1000; i++)
-    // {
-    //     cout << dost[i] << endl;
-    // }
+void printArray(int arr[], int size) {
+  cout << "Printing The Array" << endl;
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "Printing Done" << endl;
+}
 
-    return 0;
+int main() {
+  //! entire array initilize with same value
+  // int dost[1000] = {-1};
+  // fill_n(dost, 1000, 3);
+  // for (int i = 0; i < 1000; i++)
+  // {
+  //     cout << dost[i] << endl;
+  // }
+
+  //! Using Array with functions
+  //      int third[15] = {2, 7};
+  //      printArray(third, 15);
+  //
+  //      int fourth[10] = {0};
+  //      printArray(fourth, 10);
+  //
+  //      int fifth[10] = {1};
+  //      printArray(fifth, 10);
+  //
+  //      int fifthSize = sizeof(fifth) / sizeof(int);
+  //      cout << " Size of Fifth is " << fifthSize << endl;
+
+  return 0;
 }
