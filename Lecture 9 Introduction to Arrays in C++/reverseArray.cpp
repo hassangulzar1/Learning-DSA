33594c8729d98c563e859c221349450637ef9f60
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-26 23:25:52 +0500

    lecture 9 done

--- Lecture 9 Introduction to Arrays in C++/reverseArray.cpp
+++ Lecture 9 Introduction to Arrays in C++/reverseArray.cpp
@@ -6,7 +6,6 @@ int main() {
 
   int start = 0;
   int end = 4;
-
   while (start <= end) {
     swap(arr[start], arr[end]);
     // int startVal = arr[start];
@@ -16,19 +15,7 @@ int main() {
     end--;
   }
 
-  //   for (int i = 0, j = 6; i < 6 && j > 0; i++, j--) {
-  //     if (i > j) {
-  //       break;
-  //     }
-  //
-  //     int start = arr[i];
-  //     int end = arr[j];
-  //     arr[i] = end;
-  //     arr[j] = start;
-  //   }
-
   for (int i = 0; i < 5; i++) {
-    /* code */
     cout << arr[i] << endl;
   }
 }
