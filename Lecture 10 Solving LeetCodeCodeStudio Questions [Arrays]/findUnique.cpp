cc807b3b7ccbb6c1c67129aca4d490a6d5812034
hassangulzar1 <hassangulzar121@gmail.com>
2023-12-27 23:16:42 +0500

    find unique done

--- Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/findUnique.cpp
+++ Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/findUnique.cpp
@@ -2,20 +2,29 @@
 using namespace std;
 
 int main() {
+  //! BRUTEFORCE SOLUTION
+  //   int arr[7] = {1, 1, 2, 2, 3, 3, 5};
+  //
+  //   int exist = 0;
+  //   for (int i = 0; i < 7; i++) {
+  //     for (int j = 0; j < 7; j++) {
+  //       if (arr[i] == arr[j]) {
+  //         exist++;
+  //       }
+  //     }
+  //
+  //     if (exist == 1) {
+  //       cout << arr[i] << endl;
+  //     }
+  //
+  //     exist = 0;
+  //   }
+
+  //! OPTIMIZE SOLUTION
+  int ans = 0;
   int arr[5] = {1, 1, 2, 2, 3};
-
-  int exist = 0;
   for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
-      if (arr[i] == arr[j]) {
-        exist++;
-      }
-    }
-
-    if (exist == 1) {
-      cout << arr[i] << endl;
-    }
-
-    exist = 0;
+    ans = ans ^ arr[i];
   }
+  cout << ans << endl;
 }
