e6ae75aaf70b846068d3923d7fcbc20dc7cf336e
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-02 16:25:39 +0500

    more questions done

---
+++ Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/findDuplicates.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+  //   int arr[5] = {4, 2, 1, 3, 1};
+  //
+  //   int ans = 0;
+  //   for (int i = 0; i < arr.size(); i++) {
+  //     ans = ans ^ arr[i];
+  //   }
+  //
+  //   for (int i = 0; i < arr.size(); i++) {
+  //     ans = ans ^ i;
+  //   }
+  //   return ans;
+}
--- Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/uniqueNumber.cpp
+++ Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/uniqueNumber.cpp
@@ -2,36 +2,68 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-
 using namespace std;
+
 int main() {
+  //   vector<int> ans;
+  //
+  //   int count = 1;
+  //   int arr[6] = {1, 2, 2, 1, 1, 3};
+  //   for (int i = 0; i < 6; i++) {
+  //     if (arr[i] == 0) continue;
+  //
+  //     for (int j = i + 1; j < 6; j++) {
+  //       if (arr[i] == arr[j]) {
+  //         arr[j] = 0;
+  //         count++;
+  //       }
+  //     }
+  //
+  //     ans.push_back(count);
+  //     count = 1;
+  //     arr[i] = 0;
+  //   }
+  //   sort(ans.begin(), ans.end());
+  //   for (int i = 0; i < ans.size(); i++) {
+  //     cout << ans[i] << " ";
+  //
+  //     if (ans[i] == ans[i + 1]) {
+  //       cout << "false" << endl;
+  //   return false;
+  //     }
+  //   }
+  //
+  //   cout << "true" << endl;
+  //   return true;
+
+  //! Solved Code
+
   vector<int> ans;
 
-  int count = 1;
-  int arr[6] = {1, 2, 2, 1, 1, 3};
-  for (int i = 0; i < 6; i++) {
-    if (arr[i] == 0) continue;
+  int arr[5] = {1, 2, 3, 4, 5};
 
-    for (int j = i + 1; j < 6; j++) {
-      if (arr[i] == arr[j]) {
-        arr[j] = 0;
-        count++;
+  for (int i = 0; i < 5; ++i) {
+    if (arr[i] != 0) {
+      int count = 1;
+
+      for (int j = i + 1; j < 5; ++j) {
+        if (arr[i] == arr[j]) {
+          arr[j] = 0;
+          ++count;
+        }
       }
+
+      ans.push_back(count);
     }
-    ans.push_back(count);
-    count = 1;
-    arr[i] = 0;
   }
+
   sort(ans.begin(), ans.end());
-  for (int i = 0; i < ans.size(); i++) {
-    cout << ans[i] << " ";
 
+  for (int i = 0; i < ans.size() - 1; ++i) {
     if (ans[i] == ans[i + 1]) {
-      cout << "false" << endl;
-      //   return false;
+      return false;
     }
   }
 
-  cout << "true" << endl;
-  //   return true;
+  return true;
 }
