467802be52ace7e86ef41b011e34ce2ee5efa82b
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-03 20:23:24 +0500

    insert sorted array done

---
+++ Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/insertionSortedArr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  //   int i = 0;
+  //   int j = 0;
+  //   vector<int> ans;
+  //
+  //   while (i < n && j < m) {
+  //     if (arr1[i] == arr2[j]) {
+  //       ans.push_back(arr1[i]);
+  //       i++;
+  //       j++;
+  //     } else if (arr1[i] < arr2[j]) {
+  //       i++;
+  //     } else {
+  //       j++;
+  //     }
+  //   }
+  //   return ans;
+}
