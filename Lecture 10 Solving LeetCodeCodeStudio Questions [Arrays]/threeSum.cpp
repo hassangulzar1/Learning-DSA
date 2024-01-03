56754ff7bbd10142d1378a0047f0c6bb31697c5c
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-03 23:45:31 +0500

    sort 0 1 done

---
+++ Lecture 10 Solving LeetCodeCodeStudio Questions [Arrays]/threeSum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+  //   vector<int> arr = {10, 5, 5, 5, 2};
+  //   vector<int> ans;
+  //   int K = 12;
+  //
+  //   for (int i = 0; i < arr.size(); i++) {
+  //     for (int j = i + 1; j < arr.size(); j++) {
+  //       for (int k = j + 1; k < arr.size(); k++) {
+  //         if (arr[i] + arr[j] + arr[k] == K) {
+  //           ans.push_back(arr[i]);
+  //           ans.push_back(arr[j]);
+  //           ans.push_back(arr[k]);
+  //         }
+  //       }
+  //     }
+  //   }
+  //
+  //   for (int i : ans) {
+  //     cout << i << " ";
+  //   }
+}
