e1cdaf9d1f1c58f69522f5122e2ff335e4d41266
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-06 20:37:41 +0500

    peak index done

---
+++ Lecture 13 Binary Search Interview Questions [Google, Amazon, Microsoft]  ProblemSet - 1/peakIndex.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //   int s = 0;
+  //   int e = arr.size() - 1;
+  //   int mid = s + (e - s) / 2;
+  //
+  //   while (s < e) {
+  //     if (arr[mid] < arr[mid + 1]) {
+  //       s = mid + 1;
+  //     } else {
+  //       e = mid;
+  //     }
+  //     mid = s + (e - s) / 2;
+  //   }
+  //   return s;
+}
