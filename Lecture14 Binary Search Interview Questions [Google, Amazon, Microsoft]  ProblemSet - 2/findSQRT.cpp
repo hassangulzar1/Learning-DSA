c4664131226a2e81c9aa0f7e56a4d9881060d57f
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-09 20:09:51 +0500

    binary search questions completed at aggressive coe

--- Lecture14 Binary Search Interview Questions [Google, Amazon, Microsoft]  ProblemSet - 2/findSQRT.cpp
+++ Lecture14 Binary Search Interview Questions [Google, Amazon, Microsoft]  ProblemSet - 2/findSQRT.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-int main() {
-  int n = 22;
+int binarySearch(int n) {
   int s = 0;
   int e = n;
   long long int mid = s + (e - s) / 2;
@@ -23,4 +22,29 @@ int main() {
     mid = s + (e - s) / 2;
   }
   return ans;
+}
+
+double morePrecision(int n, int precision, int tempSol) {
+  double factor = 1;
+  double ans = tempSol;
+
+  for (int i = 0; i < precision; i++) {
+    factor = factor / 10;
+
+    for (double j = ans; j * j < n; j = j + factor) {
+      ans = j;
+    }
+  }
+
+  return ans;
+}
+int main() {
+  int n;
+  cout << " Enter the number" << endl;
+
+  cin >> n;
+
+  int tempSol = binarySearch(n);
+  cout << "Anser is " << morePrecision(n, 3, tempSol);
+  return 0;
 }
