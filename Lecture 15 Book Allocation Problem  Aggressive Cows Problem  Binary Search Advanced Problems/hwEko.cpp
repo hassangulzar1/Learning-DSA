968771f554a34407b835ffce1857f86e29bda001
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-11 14:56:07 +0500

    eko spoj problem solved

--- Lecture 15 Book Allocation Problem  Aggressive Cows Problem  Binary Search Advanced Problems/hwEko.cpp
+++ Lecture 15 Book Allocation Problem  Aggressive Cows Problem  Binary Search Advanced Problems/hwEko.cpp
@@ -1,44 +1,42 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
 
-bool isPossible(vector<int> arr, int m, int mid) {
-  int sum = 0;
+bool isPossible(int arr[], int n, int m, int mid) {
+  long long int sum = 0;
 
-  for (int i = 0; i < arr.size(); i++) {
-    if (arr[i] <= mid) {
-      continue;
-    } else {
-      sum += arr[i] - mid;
-    }
+  for (int i = 0; i < n; i++) {
+    if (arr[i] > mid) sum += arr[i] - mid;
   }
 
-  if (sum <= m) {
+  if (sum >= m) {
     return true;
   }
   return false;
 }
 
 int main() {
-  vector<int> arr = {4, 42, 40, 26, 46};
-  int m = 20;
-  sort(arr.begin(), arr.end());
+  long long int n, m;
+  cin >> n >> m;
+  int a[n];
+
+  for (int i = 0; i < n; i++) cin >> a[i];
 
   int s = 0;
-  int e = arr[arr.size() - 1];
+  int e = *max_element(a, a + n);
   int mid = s + (e - s) / 2;
-  int ans = -1;
+  long long int ans = 0;
   while (s <= e) {
-    if (isPossible(arr, m, mid)) {
+    if (isPossible(a, n, m, mid)) {
       ans = mid;
-      e = mid - 1;
-    } else {
       s = mid + 1;
+    } else {
+      e = mid - 1;
     }
 
     mid = s + (e - s) / 2;
   }
 
   cout << ans << endl;
+  return 0;
 }
