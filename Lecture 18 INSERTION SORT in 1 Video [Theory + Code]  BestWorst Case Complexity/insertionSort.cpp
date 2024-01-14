5d2aa875cd08d69714663227b4968ea9ebfe7de0
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-14 22:49:52 +0500

    insertion sort problem solved

---
+++ Lecture 18 INSERTION SORT in 1 Video [Theory + Code]  BestWorst Case Complexity/insertionSort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //   for (int i = 1; i < n; i++) {
+  //     int temp = arr[i];
+  //
+  //     int j = i - 1;
+  //     while (j >= 0) {
+  //       if (arr[j] > temp) {
+  //         arr[j + 1] = arr[j];
+  //       } else {
+  //         break;
+  //       }
+  //       j--;
+  //     }
+  //     arr[j + 1] = temp;
+  //   }
+}
