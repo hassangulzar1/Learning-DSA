36354d2eb8435bd50c67ad65c5b81a924680bac5
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-21 19:41:18 +0500

    input and output in java done

--- java/2- first java program/Main.java
+++ java/2- first java program/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hellow world!");
+        System.out.println(args[1]);
+        System.out.println("Hey how are you?");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(input.nextLine());
+
     }
 
 }
