73e938e78cf3f66d0b3600183ba45add28e5d4e1
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-22 16:28:30 +0500

    func overloading

---
+++ java/4- methods/FunctionOverloading.java
@@ -0,0 +1,14 @@
+public class FunctionOverloading {
+    public static void main(String[] args) {
+        fun(22);
+        fun("hassan");
+    }
+
+    static void fun(int a) {
+
+    }
+
+    static void fun(String name) {
+
+    }
+}
--- java/4- methods/VarArgs.java
+++ java/4- methods/VarArgs.java
@@ -3,10 +3,15 @@ import java.util.Arrays;
 public class VarArgs {
 
     public static void main(String[] args) {
-        fun(2, 3, 4, 5, 6);
+        // fun(2, 3, 4, 5, 6);
+        multiple(1, 2, "sdf", "dsf");
     }
 
     static void fun(int... v) {
         System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a, int b, String... v) {
+
     }
 }
