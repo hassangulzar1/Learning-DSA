89db27fac376d275b6c4ba596a443ebbb3f7d2f3
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-23 20:38:36 +0500

    reverse an array

--- java/5- Array/Swap.java
+++ java/5- Array/Swap.java
@@ -3,9 +3,19 @@ import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 54, 45, 18 };
-        swap(arr, 1, 3);
+        reverse(arr);
 
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr) {
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s < e) {
+            swap(arr, s++, e--);
+        }
+
     }
 
     static void swap(int[] arr, int index1, int index2) {
