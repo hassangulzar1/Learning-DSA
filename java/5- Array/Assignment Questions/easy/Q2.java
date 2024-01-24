f79dd7b101ae09aca32a66197453de3e7e560e78
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-24 16:49:50 +0500

    third question done

--- java/5- Array/Assignment Questions/easy/Q2.java
+++ java/5- Array/Assignment Questions/easy/Q2.java
@@ -5,15 +5,23 @@ public class Q2 {
         int nums[] = { 1, 2, 1 };
         int[] ans = new int[nums.length * 2];
 
-        for (int i = 1; i <= ans.length; i++) {
-            if (i > ans.length / 2) {
-                ans[i - 1] = nums[(i - ans.length / 2) - 1];
+        // ! brute force approch
+        // for (int i = 1; i <= ans.length; i++) {
+        // if (i > ans.length / 2) {
+        // ans[i - 1] = nums[(i - ans.length / 2) - 1];
+        //
+        // } else {
+        // ans[i - 1] = nums[i - 1];
+        // }
+        // }
 
-            } else {
-                ans[i - 1] = nums[i - 1];
-            }
+        // ! Optimize solution approch
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
         }
 
+        // System.gc();
         System.out.println(Arrays.toString(ans));
     }
 }
