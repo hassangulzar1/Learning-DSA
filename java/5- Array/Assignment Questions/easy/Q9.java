d677408ebb23a0de1b3a8ac18a364c80dc1ae8e2
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-25 18:26:26 +0500

    10th question done

--- java/5- Array/Assignment Questions/easy/Q9.java
+++ java/5- Array/Assignment Questions/easy/Q9.java
@@ -1,21 +1,22 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Q9 {
     public static void main(String[] args) {
         int[] nums = { 0, 1, 2, 3, 4 };
         int[] index = { 0, 1, 2, 2, 1 };
-
+        ArrayList<Integer> list = new ArrayList<>(nums.length);
         int[] ans = new int[nums.length];
 
-        int j = ans.length - 2;
-        int i = 0;
-        while (i < ans.length && j > 0) {
-            ans[j + 1] = nums[j--];
-            ans[j - 1] = ans[j + 1];
-            ans[index[i]] = nums[i++];
+        for (int i = 0; i < ans.length; i++) {
+            list.add(index[i], nums[i]);
         }
 
-        System.out.println(Arrays.toString(ans));
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = list.get(i);
+        }
+
+        System.out.println(list);
 
     }
 }
