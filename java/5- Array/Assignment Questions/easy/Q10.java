b789f2d30458b047d238ba55f8b6e15802fa6bcd
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-25 19:59:18 +0500

    q11 done

--- java/5- Array/Assignment Questions/easy/Q10.java
+++ java/5- Array/Assignment Questions/easy/Q10.java
@@ -1,18 +1,31 @@
 public class Q10 {
     public static void main(String[] args) {
         String sentence = "thequickbrownfoxjumpsoverthelazydog";
-        int[] allCharCount = new int[26];
 
-        for (int i = 0; i < sentence.length(); i++) {
-            allCharCount[sentence.charAt(i) - 'a']++;
-        }
+        // ! brute force solution
+        // int[] allCharCount = new int[26];
+        // for (int i = 0; i < sentence.length(); i++) {
+        // allCharCount[sentence.charAt(i) - 'a']++;
+        // }
+        //
+        // for (int i = 0; i < allCharCount.length; i++) {
+        // if (allCharCount[i] == 0) {
+        // System.out.println("false");
+        // }
+        // }
 
-        for (int i = 0; i < allCharCount.length; i++) {
-            if (allCharCount[i] == 0) {
-                System.out.println("false");
-            }
-        }
+        // ! optimise solution
+        // char ch = 'a';
+        //
+        // for (int i = 0; i < 26; i++) {
+        //
+        // if (sentence.contains(String.valueOf(ch))) {
+        // ch++;
+        // } else {
+        // System.out.println("false");
+        // }
+        // }
+        // System.out.println("true");
 
-        System.out.println("true");
     }
 }
---
+++ java/5- Array/Assignment Questions/easy/Q11.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q11 {
+    public static void main(String[] args) {
+        // String[][] items = {
+        // { "phone", "blue", "pixel" },
+        // { "computer", "silver", "lenovo" },
+        // { "phone", "gold", "iphone" }
+        // };
+        List<List<String>> items = new ArrayList<>();
+
+        items.add(Arrays.asList("phone", "gold", "iphone"));
+        items.add(Arrays.asList("computer", "silver", "lenovo"));
+        items.add(Arrays.asList("phone", "blue", "pixel"));
+
+        String ruleKey = "color";
+        String ruleValue = "silver";
+
+        // ! BruteForce Solution
+        // int count = 0;
+        // for (int i = 0; i < items.size(); i++) {
+        // List<String> temp = items.get(i);
+        //
+        // if ("type".equals(ruleKey)) {
+        // count += temp.get(0).equals(ruleValue) ? 1 : 0;
+        // }
+        // if ("color".equals(ruleKey)) {
+        // count += temp.get(1).equals(ruleValue) ? 1 : 0;
+        // }
+        // if ("name".equals(ruleKey)) {
+        // count += temp.get(2).equals(ruleValue) ? 1 : 0;
+        // }
+        // }
+        // System.out.println(count);
+
+        // ! Optimise Solution
+
+        // int count = 0;
+        // for (int i = 0; i < items.size(); i++) {
+        // if ("type".equals(ruleKey) && items.get(i).get(0).equals(ruleValue))
+        // count++;
+        // if ("color".equals(ruleKey) && items.get(i).get(1).equals(ruleValue))
+        // count++;
+        // if ("name".equals(ruleKey) && items.get(i).get(2).equals(ruleValue))
+        // count++;
+        //
+        // }
+    }
+}
