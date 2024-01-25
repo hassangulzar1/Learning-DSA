06e57a22637a9e3b5ccb7b6003a61a4e9f59cf1b
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-25 22:52:10 +0500

    q14 done

--- java/5- Array/Assignment Questions/easy/Q14.java
+++ java/5- Array/Assignment Questions/easy/Q14.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class Q14 {
     public static void main(String[] args) {
+        int m = 2;
+        int n = 3;
+        int[][] indices = { { 0, 1 }, { 1, 1 } };
 
+        int[][] arr = new int[m][n];
+        int countOdd = 0;
+        for (int i = 0; i < indices.length; i++) {
+            int rowIndexIncri = indices[i][0];
+            int colIndexIncri = indices[i][1];
+
+            for (int j = 0; j < arr[rowIndexIncri].length; j++)
+                arr[rowIndexIncri][j]++;
+
+            for (int j = 0; j < arr.length; j++)
+                arr[j][colIndexIncri]++;
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] % 2 != 0)
+                    countOdd++;
+            }
+        }
+        System.out.println(countOdd);
     }
 }
