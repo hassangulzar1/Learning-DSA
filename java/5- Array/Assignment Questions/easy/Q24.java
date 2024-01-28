b72ab37ee30f9339c6c03e1ea9b109a8aa3a4475
hassangulzar1 <hassangulzar121@gmail.com>
2024-01-29 00:39:42 +0500

    all easy question done

--- java/5- Array/Assignment Questions/easy/Q24.java
+++ java/5- Array/Assignment Questions/easy/Q24.java
@@ -1,11 +1,19 @@
 public class Q24 {
     public static void main(String[] args) {
-        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int[] nums = { -1 };
 
-        int sum = Integer.MIN_VALUE;
-
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
 
         }
+
+        System.out.println(max);
     }
 }
