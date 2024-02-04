84d9c46002c54773a712575b0a18cdc5749e8311
hassangulzar1 <hassangulzar121@gmail.com>
2024-02-04 15:20:57 +0500

    q5 done

--- java/5- Array/Assignment Questions/medium/Q5.java
+++ java/5- Array/Assignment Questions/medium/Q5.java
@@ -2,26 +2,66 @@ import java.util.Arrays;
 
 public class Q5 {
     public static void main(String[] args) {
+        // int[] nums = { 1, 2, 3, 4 };
+        // int[] ans = new int[nums.length];
+        // int[] output = new int[nums.length];
+        // int[] result = new int[nums.length];
+        //
+        // int leftPro = 1;
+        // ans[0] = 1;
+        // for (int i = 0; i < ans.length - 1; i++) {
+        // leftPro *= nums[i];
+        // ans[i + 1] = leftPro;
+        // }
+        //
+        // int rightPro = 1;
+        // output[0] = 1;
+        // for (int i = nums.length - 1; i > 0; i--) {
+        // rightPro *= nums[i];
+        // output[(nums.length - 1) - i + 1] = rightPro;
+        // }
+        //
+        // for (int i = 0; i < output.length; i++) {
+        // result[i] = ans[i] * output[(nums.length - 1) - i];
+        // }
+
+        // ! optimising that solution
+        // int[] nums = { 1, 2, 3, 4 };
+        // int[] ans = new int[nums.length];
+        // int[] output = new int[nums.length];
+        //
+        // int leftPro = 1;
+        // ans[0] = 1;
+        // for (int i = 0; i < ans.length - 1; i++) {
+        // leftPro *= nums[i];
+        // ans[i + 1] = leftPro;
+        // }
+        //
+        // int rightPro = 1;
+        // output[0] = 1;
+        // for (int i = nums.length - 1; i > 0; i--) {
+        // rightPro *= nums[i];
+        // output[(nums.length - 1) - i + 1] = rightPro;
+        // }
+        //
+        // for (int i = 0; i < nums.length; i++) {
+        // nums[i] = ans[i] * output[(nums.length - 1) - i];
+        // }
+
+        // ! more optimised code
         int[] nums = { 1, 2, 3, 4 };
-        int[] ans = new int[nums.length];
+        int[] output = new int[nums.length];
 
-        int i = 0;
-        while (i < nums.length) {
-            int leftPro = i == 0 ? 0 : 1;
-
-            for (int j = 0; j < i; j++) {
-                leftPro *= nums[j];
-            }
-
-            int righPro = i == nums.length - 1 ? 0 : 1;
-            for (int j = i + 1; j <= ans.length - 1; j++) {
-                righPro *= nums[j];
-
-            }
-
-            ans[i++] = righPro * leftPro;
-
+        int product = 1;
+        for (int i = 0; i < output.length; i++) {
+            output[i] = product;
+            product *= nums[i];
         }
-        System.out.println(Arrays.toString(ans));
+        product = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= product;
+            product *= nums[i];
+        }
+        System.out.println(Arrays.toString(output));
     }
 }
