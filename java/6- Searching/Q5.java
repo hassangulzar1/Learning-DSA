0c1b275ddffdb0f7af155924f462cc3246682a29
Hassan <hassangulzar121@gmail.com>
2024-02-15 15:54:29 +0500

    Q6 done

---
+++ java/6- Searching/Q5.java
@@ -0,0 +1,43 @@
+public class Q5 {
+    static int binarySearch(int[] arr, int target, int s, int e) {
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+
+        }
+        return -1;
+
+    }
+
+    static int findingRange(int[] arr, int target) {
+
+        int start = 0;
+        int end = 1;
+        // condition
+
+        while (target > arr[end]) {
+            int newStart = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = newStart;
+        }
+
+        return binarySearch(arr, target, start, end);
+
+    }
+
+    public static void main(String[] args) {
+        // First Finding the Range
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        int target = 9;
+
+        System.out.println(findingRange(arr, target));
+
+    }
+}
---
+++ java/6- Searching/Q6.java
@@ -0,0 +1,21 @@
+public class Q6 {
+    public static void main(String[] args) {
+
+        int[] arr = { 0, 10, 5, 2 };
+
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+
+        System.out.println(s);
+    }
+}
