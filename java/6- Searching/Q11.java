066d3e54f6b0811d475501236be3ccb9f64ef8c3
Hassan <hassangulzar121@gmail.com>
2024-02-26 16:38:50 +0500

    q11 done

--- java/6- Searching/Q11.java
+++ java/6- Searching/Q11.java
@@ -2,4 +2,42 @@ public class Q11 {
     public static void main(String[] args) {
 
     }
+
+    public int splitArray(int[] nums, int k) {
+        int s = 0;
+        int e = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            s = Math.max(s, nums[i]); // in the end of the loop this will contain max item of array
+            e += nums[i];
+        }
+
+        // binary search
+
+        while (s < e) {
+
+            int mid = s + (e - s) / 2;
+
+            int sum = 0;
+            int pices = 1;
+
+            for (int num : nums) {
+                if (sum + num > mid) {
+                    sum = num;
+                    pices++;
+
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pices > k) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+
+        return e;
+    }
 }
