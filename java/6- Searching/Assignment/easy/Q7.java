fb2c383c72c5d19156179d7f2742aa246bcbed69
Hassan <hassangulzar121@gmail.com>
2024-02-28 18:58:08 +0500

    q7 done

--- java/6- Searching/Assignment/easy/Q7.java
+++ java/6- Searching/Assignment/easy/Q7.java
@@ -1,5 +1,37 @@
 public class Q7 {
     public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 7, 11 };
+        int k = 5;
 
+        // ! linear Search
+        // int index = 0;
+        // for (int i = 1; i <= arr[arr.length - 1]; i++) {
+        // if (arr[index] != i) {
+        // k--;
+        // if (k == 0)
+        // return i;
+        // } else {
+        // index++;
+        // }
+        // }
+
+        // !Binary Search
+
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (arr[mid] - mid - 1 < k) {
+                s = mid + 1;
+
+            } else {
+                e = mid - 1;
+            }
+
+        }
+
+        System.out.println(s + k);
     }
 }
