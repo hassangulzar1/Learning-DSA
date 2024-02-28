8188bb148eeffc99a7706ae3fa12d11fe9bf7ba1
Hassan <hassangulzar121@gmail.com>
2024-02-28 17:04:07 +0500

    q6 done

--- java/6- Searching/Assignment/easy/Q6.java
+++ java/6- Searching/Assignment/easy/Q6.java
@@ -1,5 +1,17 @@
 public class Q6 {
     public static void main(String[] args) {
+        int n = 5;
+        long s = 0;
+        long e = n;
+        while (s <= e) {
+            long mid = s + (e - s) / 2;
 
+            if (mid * (mid + 1) / 2 <= n) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        System.out.println(e);
     }
 }
