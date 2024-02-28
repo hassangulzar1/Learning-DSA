4e08197548bb4f73bad43e6007bb02d19018a0fc
Hassan <hassangulzar121@gmail.com>
2024-02-28 15:35:58 +0500

    by using binary search qno 4 solved

--- java/6- Searching/Assignment/easy/Q4.java
+++ java/6- Searching/Assignment/easy/Q4.java
@@ -7,17 +7,43 @@ public class Q4 {
         int target = 200;
 
         // ! two pointers Approch
+        // int s = 0;
+        // int e = numbers.length - 1;
+        // while (numbers[s] + numbers[e] != target) {
+        // if (numbers[s] + numbers[e] < target)
+        // s++;
+        // else {
+        // e--;
+        // }
+        //
+        // }
+        // System.out.println(Arrays.toString(new int[] { s + 1, e + 1 }));
+
+        // ! by binary search
         int s = 0;
         int e = numbers.length - 1;
-        while (numbers[s] + numbers[e] != target) {
-            if (numbers[s] + numbers[e] < target)
-                s++;
-            else {
-                e--;
-            }
 
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            int sum = numbers[s] + numbers[e];
+            if (sum == target) {
+                return new int[] { s + 1, e + 1 };
+            } else if (sum < target) {
+                if (numbers[e] + numbers[mid] < target) {
+                    s = mid + 1;
+
+                } else {
+                    s++;
+                }
+            } else {
+                if (numbers[s] + numbers[mid] > target) {
+                    e = mid - 1;
+
+                } else {
+                    e--;
+                }
+            }
         }
-        System.out.println(Arrays.toString(new int[] { s + 1, e + 1 }));
     }
 
 }
