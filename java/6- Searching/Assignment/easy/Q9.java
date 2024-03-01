5af715376fbad66bd41b61759013ae406fa311bb
Hassan <hassangulzar121@gmail.com>
2024-03-01 22:48:46 +0500

    done

---
+++ java/6- Searching/Assignment/easy/Q9.java
@@ -0,0 +1,20 @@
+public class Q9 {
+    public static void main(String[] args) {
+
+        int[] arr = { 0, 10, 5, 2 };
+        int s = 0;
+        int e = arr.length - 1;
+
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                s = mid + 1;
+
+            } else {
+                e = mid;
+            }
+        }
+        System.out.println(e);
+    }
+}
