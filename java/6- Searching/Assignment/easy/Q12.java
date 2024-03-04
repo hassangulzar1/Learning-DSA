b2f209a80d37c08196e72666677c3a9ff137d11b
Hassan <hassangulzar121@gmail.com>
2024-03-05 00:45:42 +0500

    q16 done

--- java/6- Searching/Assignment/easy/Q12.java
+++ java/6- Searching/Assignment/easy/Q12.java
@@ -1,19 +1,25 @@
+import java.util.ArrayList;
+
 public class Q12 {
     public static void main(String[] args) {
 
         int[] nums1 = { 1, 2, 2, 1 }, nums2 = { 2, 2 };
-        int[] temp = new int[1001];
-        int k = 0;
+        // int[] temp = new int[1001];
+        // int k = 0;
+        //
+        // for (int n : nums1) {
+        // temp[n] = 1;
+        // }
+        //
+        // for (int n : nums2) {
+        //
+        // if (temp[n] == 1) {
+        //
+        // }
+        // }
 
-        for (int n : nums1) {
-            temp[n] = 1;
-        }
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        for (int n : nums2) {
-
-            if (temp[n] == 1) {
-
-            }
-        }
+        
     }
 }
---
+++ java/6- Searching/Assignment/easy/Q16.java
@@ -0,0 +1,22 @@
+public class Q16 {
+    public static void main(String[] args) {
+        int[] nums = { -1, 0, 3, 5, 9, 12 };
+        int target = 9;
+
+        int s = 0;
+        int e = nums.length - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target) {
+                System.out.println(mid);
+            } else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+
+        }
+        System.out.println(-1);
+    }
+}
