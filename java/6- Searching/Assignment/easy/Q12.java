06ee79ca1f9828d89314b4ba68b9513569a8789f
Hassan <hassangulzar121@gmail.com>
2024-03-05 19:50:33 +0500

    q12 done

--- java/6- Searching/Assignment/easy/Q12.java
+++ java/6- Searching/Assignment/easy/Q12.java
@@ -1,25 +1,58 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Q12 {
+    public static int binarySearch(int[] arr, int target, int st) {
+
+        int s = st;
+        int e = arr.length - 1;
+
+        int ifPossible = -1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (arr[mid] <= target) {
+                ifPossible = mid;
+                s = mid + 1;
+
+            } else {
+                e = mid - 1;
+            }
+        }
+
+        return ifPossible;
+    }
+
     public static void main(String[] args) {
 
-        int[] nums1 = { 1, 2, 2, 1 }, nums2 = { 2, 2 };
-        // int[] temp = new int[1001];
-        // int k = 0;
-        //
-        // for (int n : nums1) {
-        // temp[n] = 1;
-        // }
-        //
-        // for (int n : nums2) {
-        //
-        // if (temp[n] == 1) {
-        //
-        // }
-        // }
+        int[] nums1 = { 4, 9, 5 }, nums2 = { 9, 4, 9, 8, 4 };
+        Arrays.sort(nums2);
+        Arrays.sort(nums1);
+        ArrayList<Integer> temp = new ArrayList<>();
 
-        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
 
-        
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                temp.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        int[] ans = new int[temp.size()];
+
+        for (int k = 0; k < ans.length; k++) {
+
+            ans[k] = temp.get(k);
+
+        }
+
+        return ans;
     }
 }
