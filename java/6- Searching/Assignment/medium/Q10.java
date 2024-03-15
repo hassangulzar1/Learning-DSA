11b057ff8a181df8a2d75e98b9b4b0a03a199d74
Hassan <hassangulzar121@gmail.com>
2024-03-15 17:42:41 +0500

    q10 on doing

--- java/6- Searching/Assignment/medium/Q10.java
+++ java/6- Searching/Assignment/medium/Q10.java
@@ -1,42 +1,38 @@
-import java.util.Arrays;
-
 public class Q10 {
 
-    public static int check(int[] arr, long target) {
+    public static boolean check(int[] arr, long target, int h) {
 
-        int index = 0;
         int hour = 0;
-
-        while (index != arr.length) {
-            double num1 = arr[index];
-            double num2 = target;
-            hour += (int) Math.ceil(num1 / num2);
-
-            index++;
+        for (int i = 0; i < arr.length; i++) {
+            hour += arr[i] / target + (arr[i] % target == 0 ? 0 : 1);
+        }
+        if (hour <= h) {
+            return true;
 
         }
-        return hour;
+        return false;
     }
 
     public static void main(String[] args) {
-        int[] piles = { 312884470 };
-        int h = 312884469;
-        Arrays.sort(piles);
+        int[] piles = { 3, 6, 7, 11 };
+        int h = 8;
+        int maxi = 0;
+        for (int i = 0; i < piles.length; i++) {
+            maxi = Math.max(maxi, piles[i]);
+        }
 
-        long s = piles[0];
-        long e = piles[piles.length - 1];
-        int isPossible = -1;
+        long s = 1;
+        long e = maxi;
 
-        while (s <= e) {
+        while (s < e) {
             long mid = s + (e - s) / 2;
-            if (check(piles, mid) <= h) {
-                isPossible = (int) mid;
-                e = mid - 1;
+            if (check(piles, mid, h)) {
+                e = mid;
             } else {
                 s = mid + 1;
             }
         }
 
-        System.out.println(isPossible);
+        System.out.println(s);
     }
 }
