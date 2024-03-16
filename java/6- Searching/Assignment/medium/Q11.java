dbb09d6ad4b05be69ae0f7e4c5aca6898a7ccab0
Hassan <hassangulzar121@gmail.com>
2024-03-16 15:57:09 +0500

    working on this

--- java/6- Searching/Assignment/medium/Q11.java
+++ java/6- Searching/Assignment/medium/Q11.java
@@ -1,49 +1,47 @@
 import java.util.Arrays;
 
 public class Q11 {
+    public static int findClosest(int[] sortedNums, int target) {
+        int left = 0, right = sortedNums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (sortedNums[mid] == target)
+                return target;
+            else if (sortedNums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        if (left > 0 && Math.abs(sortedNums[left] - target) > Math.abs(sortedNums[left - 1] - target))
+            return sortedNums[left - 1];
+        return sortedNums[left];
+
+    }
+
     public static void main(String[] args) {
         int[] nums1 = { 1, 10, 4, 4, 2, 7 }, nums2 = { 9, 3, 5, 1, 7, 4 };
 
-        int subtract = 0;
+        int mod = 1000000007;
 
-        int maxiIndex = -1;
-        int val = Integer.MIN_VALUE;
+        int n = nums1.length;
+        int[] sortedNums1 = nums1.clone();
+        Arrays.sort(sortedNums1);
 
-        for (int i = 0; i < nums1.length; i++) {
-            int subtractValue = Math.abs(nums1[i] - nums2[i]);
-            subtract += subtractValue;
-
-            if (subtractValue > val) {
-                val = subtractValue;
-                maxiIndex = i;
-            }
-
+        int sumDiff = 0;
+        for (int i = 0; i < n; i++) {
+            sumDiff = (sumDiff + Math.abs(nums1[i] - nums2[i])) % mod;
         }
 
-        Arrays.sort(nums1);
-        subtract = subtract - val;
-        int s = 0;
-        int e = nums1.length - 1;
+        int minDiff = sumDiff;
 
-        // System.out.println(maxiIndex);
-        // System.out.println(val);
-
-        while (s < e) {
-            int mid = s + (e - s) / 2;
-
-            int sub = Math.abs(nums2[maxiIndex] - nums1[mid]);
-
-            int checkFront = Math.abs(nums2[maxiIndex] - nums1[mid + 1]);
-            if (sub < val && checkFront < val) {
-                val = sub;
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
+        for (int i = 0; i < n; i++) {
+            int originalValue = nums1[i];
+            int closestValue = findClosest(sortedNums1, nums2[i]);
+            int newDiff = (sumDiff - Math.abs(originalValue - nums2[i]) + Math.abs(closestValue - nums2[i])) % mod;
+            minDiff = Math.min(minDiff, newDiff);
         }
 
-        int ans = Math.abs(nums1[s] - nums2[maxiIndex]);
+        System.out.println(minDiff);
 
-        System.out.println(subtract + ans);
     }
 }
