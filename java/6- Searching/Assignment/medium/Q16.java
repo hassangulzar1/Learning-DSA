20a744a56c61aede9d7222d28f4e432a3ae670e2
Hassan <hassangulzar121@gmail.com>
2024-03-20 19:22:21 +0500

    q16 doing

--- java/6- Searching/Assignment/medium/Q16.java
+++ java/6- Searching/Assignment/medium/Q16.java
@@ -1,6 +1,28 @@
 public class Q16 {
+
+    public static int findDays(int[] arr, int target) {
+
+    }
+
     public static void main(String[] args) {
         int[] weights = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int days = 5;
+
+        int sum = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+        }
+
+        int s = 0;
+        int e = sum;
+
+        while (s < e) {
+
+            int mid = s + (e - s) / 2;
+
+            int daysSum = findDays(weights, mid);
+
+        }
     }
 }
