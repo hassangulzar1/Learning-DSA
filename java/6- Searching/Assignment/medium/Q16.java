78ee31b8d94fbbd6b7955fa0236e6a38a799097f
Hassan <hassangulzar121@gmail.com>
2024-03-20 21:31:11 +0500

    q16 done

--- java/6- Searching/Assignment/medium/Q16.java
+++ java/6- Searching/Assignment/medium/Q16.java
@@ -1,28 +1,66 @@
 public class Q16 {
 
-    public static int findDays(int[] arr, int target) {
+    // public static int findDays(int[] arr, int target, int givenDays) {
+    //
+    // int index = 0;
+    //
+    // int sum = 0;
+    // int days = 1;
+    //
+    // while (index < arr.length) {
+    //
+    // if (sum + arr[index] <= target) {
+    // sum += arr[index++];
+    //
+    // } else if (arr[index] > target) {
+    // return givenDays + 1;
+    // } else {
+    // sum = 0;
+    // days++;
+    // }
+    // }
+    // return days;
+    // }
+    public static boolean findDays(int[] arr, int target, int givenDays) {
 
+        int count = 1,
+                sum = 0;
+
+        for (int i : arr) {
+            if (i > target) {
+
+                return false;
+            }
+
+            if ((sum += i) > target) {
+                if (++count > givenDays) {
+                    return false;
+                }
+
+                sum = i;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         int[] weights = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int days = 5;
 
-        int sum = 0;
-
-        for (int i = 0; i < weights.length; i++) {
-            sum += weights[i];
-        }
-
         int s = 0;
-        int e = sum;
+        int e = 500 * weights.length / days;
 
         while (s < e) {
 
             int mid = s + (e - s) / 2;
 
-            int daysSum = findDays(weights, mid);
-
+            if (findDays(weights, mid, days)) {
+                e = mid;
+            } else {
+                s = mid + 1;
+            }
         }
+        System.out.println(s);
+
     }
 }
