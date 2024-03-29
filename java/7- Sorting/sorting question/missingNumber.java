66cd8acdbb41d8eac2a5753b63790763f1964ea2
Hassan <hassangulzar121@gmail.com>
2024-03-29 22:54:23 +0500

    q

---
+++ java/7- Sorting/sorting question/missingNumber.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class missingNumber {
+    public static void swap(int[] arr, int s, int e) {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 0, 1 };
+
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] < nums.length && nums[i] != nums[nums[i]]) {
+                swap(nums, i, nums[i]);
+            } else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j) {
+                // return j;
+                System.out.println(j);
+
+            }
+        }
+        // return nums.length;
+    }
+}
