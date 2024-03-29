676f13c84a9924102a01f6008c7423769f3fdf5f
Hassan <hassangulzar121@gmail.com>
2024-03-29 23:52:55 +0500

    cyclic sort question done

--- java/7- Sorting/sorting question/Q41.java
+++ java/7- Sorting/sorting question/Q41.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Q645 {
+public class Q41 {
     public static void swap(int[] arr, int s, int e) {
         int temp = arr[s];
         arr[s] = arr[e];
@@ -9,23 +10,26 @@ public class Q645 {
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 2, 4 };
-        List<Integer> ans = new ArrayList<>();
+        int[] nums = { 1 };
+
         int i = 0;
         while (i < nums.length) {
-            int correctIndex = nums[i] - 1;
-            if (nums[i] != nums[correctIndex]) {
+            int correctIndex = nums[i] > 0 ? nums[i] - 1 : nums[i];
+
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correctIndex]) {
                 swap(nums, i, correctIndex);
             } else {
                 i++;
             }
         }
+        System.out.println(Arrays.toString(nums));
 
         for (int j = 0; j < nums.length; j++) {
             if (nums[j] != j + 1) {
-                // return new int[]{nums[j],j + 1}
+                // return j + 1;
+                System.out.println(j + 1);
             }
         }
-        System.out.println(ans);
+        System.out.println(nums.length + 1);
     }
 }
