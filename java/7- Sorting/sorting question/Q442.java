5f3827af60fafff82b7b3b487b3347bb9d7e25b6
Hassan <hassangulzar121@gmail.com>
2024-03-29 23:25:10 +0500

    q442 done

---
+++ java/7- Sorting/sorting question/Q442.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q442 {
+    public static void swap(int[] arr, int s, int e) {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                ans.add(nums[j]);
+
+            }
+        }
+        System.out.println(ans);
+    }
+}
