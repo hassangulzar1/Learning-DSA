cea952a6eb2dc719b3ff0a22e8952972d85ed9a5
Hassan <hassangulzar121@gmail.com>
2024-03-29 15:31:06 +0500

    cyclic sort algo done

--- java/7- Sorting/CyclicSort.java
+++ java/7- Sorting/CyclicSort.java
@@ -1,5 +1,25 @@
-public class CyclicSort {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class CyclicSort {
+    public static void swap(int[] arr, int s, int e) {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+        int i = 0;
+
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
