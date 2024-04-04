8d58bef63e9f3f178354870b41108cb1f5cb22cf
Hassan <hassangulzar121@gmail.com>
2024-04-04 23:24:56 +0500

    working on q3

--- java/7- Sorting/Assignments/easy/Q3.java
+++ java/7- Sorting/Assignments/easy/Q3.java
@@ -4,5 +4,15 @@ public class Q3 {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 1 };
 
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
