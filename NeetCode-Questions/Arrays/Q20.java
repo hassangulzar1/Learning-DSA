6cf650c9df0888ea90b82330e61cf4fa915e21cf
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-04-18 15:58:27 +0500

    q20 done

--- NeetCode-Questions/Arrays/Q20.java
+++ NeetCode-Questions/Arrays/Q20.java
@@ -1,9 +1,29 @@
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Q20 {
     public static void main(String[] args) {
-        String s = "()";
-        System.out.println(Arrays.toString(s.split(s)));
+        String s = "()[]{}";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char bracket : s.toCharArray()) {
+
+            if (bracket == '(') {
+                stack.push(')');
+            } else if (bracket == '[') {
+                stack.push(']');
+            } else if (bracket == '{') {
+                stack.push('}');
+            } else {
+                if (stack.isEmpty() || stack.peek() != bracket) {
+                    // return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        System.out.println(stack.size());
 
     }
 }
