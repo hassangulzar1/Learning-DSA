f9a7c00a604a6a01acc28707f76428cfcc7fc498
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-04-18 15:27:13 +0500

    q20 working

---
+++ NeetCode-Questions/Arrays/Q20.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Q20 {
+    public static void main(String[] args) {
+        String s = "()";
+        System.out.println(Arrays.toString(s.split(s)));
+
+    }
+}
