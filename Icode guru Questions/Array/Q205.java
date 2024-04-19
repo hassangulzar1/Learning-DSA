7fff1b172292c2e2e5bcd07ba5a2a009c7dfaf8d
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-04-19 17:10:01 +0500

    working

--- Icode guru Questions/Array/Q205.java
+++ Icode guru Questions/Array/Q205.java
@@ -1,5 +1,18 @@
+import java.util.HashSet;
+
 public class Q205 {
     public static void main(String[] args) {
         String s = "egg", t = "add";
+
+        HashSet<Character> sMap = new HashSet<>();
+        HashSet<Character> tMap = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sMap.add(s.charAt(i));
+            tMap.add(t.charAt(i));
+        }
+
+        System.out.println(s.length());
+
     }
 }
---
+++ Icode guru Questions/Array/Q43.java
@@ -0,0 +1,11 @@
+public class Q43 {
+    public static void main(String[] args) {
+        String num1 = "2", num2 = "3";
+
+        int a = Integer.parseInt(num1);
+        int b = Integer.parseInt(num2);
+
+        int multiply = a * b;
+
+    }
+}
