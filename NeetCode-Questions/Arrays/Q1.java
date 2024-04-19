b6aa3c860da6da7c8f9bbc73a503cf686ae5404f
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-04-19 16:50:16 +0500

    icode guru question

---
+++ NeetCode-Questions/Arrays/Q1.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        // for (int i = 1; i < nums.length; i++) {
+        //
+        // for (int j = i; j < nums.length; j++) {
+        // if (nums[i] + nums[j - i] == target) {
+        // return new int[] { i, j - i };
+        // }
+        // }
+        // }
+        // return new int[] { -1, -1 };
+
+        // for (int i = 1; i < nums.length; i++) {
+        // for (int j = i; j < nums.length; j++) {
+        // if (nums[j] + nums[j - i] == target) {
+        // return new int[] { j - i, j };
+        // }
+        // }
+        // }
+        // return null;
+    }
+}
