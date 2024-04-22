f88da0a7588fb899bd36559b31a9e0dae2c4a05c
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-04-22 15:21:02 +0500

    solution is optimised

--- NeetCode-Questions/Arrays/Q49.java
+++ NeetCode-Questions/Arrays/Q49.java
@@ -8,25 +8,21 @@ public class Q49 {
     public static void main(String[] args) {
         String[] strs = { "a" };
 
-        // ! Brute Force solution
-        // List<List<String>> ans = new ArrayList<>();
-        // Map<String, Integer> map = new HashMap<>();
-        //
-        // for (int i = 0; i < strs.length; i++) {
-        // char[] arr = strs[i].toCharArray();
-        // Arrays.sort(arr);
-        //
-        // System.out.println();
-        // if (map.containsKey(new String(arr))) {
-        // ans.get(map.get(new String(arr))).add(strs[i]);
-        //
-        // } else {
-        // ans.add(new ArrayList<>());
-        // map.put(new String(arr), map.size());
-        // ans.get(map.get(new String(arr))).add(strs[i]);
-        // }
-        // }
-        //
-        // System.out.println(ans);
+        // ! Good Solution
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            char[] arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String sortedArray = new String(arr);
+
+            if (!map.containsKey(sortedArray)) {
+                map.put(sortedArray, new ArrayList<>());
+            }
+            map.get(sortedArray).add(strs[i]);
+
+        }
+
+        return new ArrayList<>(map.values());
     }
 }
