4ee40a606baf6ac449839f2af029121e28ae7fb7
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-04-24 14:32:14 +0500

    learning python

--- firstProgram.py
+++ firstProgram.py
@@ -1,3 +1,46 @@
+# nums1 = [1,2]
+# nums2 = [3,4]
+# 
+# merged_array = nums1 + nums2
+# merged_array.sort()
+# 
+# total_element = len(merged_array)
+# 
+# if total_element % 2 == 1:
+#     print( float(merged_array[total_element // 2]))
+# 
+# #even
+# else:
+#     middle_element1 = merged_array[total_element // 2]
+#     middle_element2 = merged_array[total_element // 2 -1]
+#   
+#     print((float (middle_element1) + float(middle_element2)) /2)
 
-name = "hassan"
-age = 22
+
+# name = input("name: ")
+# if(name == "Hassan" ):
+#     print("Hello Hassan")
+# 
+# 
+# print("yes" if name == "hassan" else "no")
+
+# str1 = "This is a string."
+# str2 = 'ApnaHassan'
+# str3 = """This is a String. \nwe are creating it in python"""
+
+# Slicing in python 
+
+# slice_thatstring = "My name is hassan"
+# 
+# print(slice_thatstring[1:10])
+
+#! Negative slicing 
+
+# str = "Apple"
+# print(str[-3:0])
+
+# inbuild String function 
+
+str = "i am a coder."
+print(str.endswith("r"))
+print(str.capitalize())
