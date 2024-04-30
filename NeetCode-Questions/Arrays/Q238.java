7673e42e8070dc6f84559d7efc3288ca55ff2d55
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-04-30 16:16:24 +0500

    one more question done

--- NeetCode-Questions/Arrays/Q238.java
+++ NeetCode-Questions/Arrays/Q238.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+
 public class Q238 {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 4 };
 
+        int[] output = new int[nums.length];
+
+        int preFix = 1;
+
+        for (int i = 0; i < output.length; i++) {
+            output[i] = preFix;
+            preFix *= nums[i];
+        }
+        preFix = 1;
+
+        for (int i = output.length - 1; i >= 0; i--) {
+            output[i] *= preFix;
+            preFix *= nums[i];
+        }
     }
 }
