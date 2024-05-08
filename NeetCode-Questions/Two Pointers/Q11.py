0be0342be3a4d1abab3513af2f1fcca3ec7f88cd
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-05-08 14:52:24 +0500

    Q11 two pointers done

---
+++ NeetCode-Questions/Two Pointers/Q11.py
@@ -0,0 +1,15 @@
+height = [1,1]
+
+s = 0
+e = len(height) -1
+
+ans = 0
+while s < e:
+    multiPlication = min(height[s],height[e])
+    ans = ans if multiPlication * (e- s) < ans else multiPlication * (e- s)
+    if height[s] < height[e]:
+        s += 1
+    else:
+        e -= 1
+
+print(ans)
