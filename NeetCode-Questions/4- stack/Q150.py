670a93c9abc16a694e3351fceecc353ec046a460
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-05-22 20:30:34 +0500

    stack q22 done

--- NeetCode-Questions/4- stack/Q150.py
+++ NeetCode-Questions/4- stack/Q150.py
@@ -2,11 +2,17 @@ tokens = ["2","1","+","3","*"]
 
 integerStack = []
 
-solved = 0
 for val in tokens:
     if val == "+":
-        integerStack.append(integerStack.pop() + integerStack.pop())
+         integerStack.append(integerStack.pop() + integerStack.pop())
     elif val == "-":
-        val1,val2= integerStack.pop(),integerStack.pop()
-        integerStack.append(val2-val1)
-
+         val1,val2= integerStack.pop(),integerStack.pop()
+         integerStack.append(val2-val1)
+    elif val == "*":
+         integerStack.append(integerStack.pop() * integerStack.pop())
+    elif val == "/":
+         val1,val2= integerStack.pop(),integerStack.pop()
+         integerStack.append(int(val2/val1) )
+    else:
+         integerStack.append(int(val))
+print(integerStack)
