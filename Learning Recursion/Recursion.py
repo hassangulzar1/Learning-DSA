0cc4bdf229be457429cd1f299b5a8ee3064adc2a
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-06-20 16:00:54 +0500

    fibbonachi num

--- Learning Recursion/Recursion.py
+++ Learning Recursion/Recursion.py
@@ -1,8 +1,15 @@
 
-def printN(n):
-    print(n)
-    if n == 5:        
-     return n
-    printN(n + 1)
+# def printN(n):
+#     print(n)
+#     if n == 5:        
+#      return n
+#     printN(n + 1)
+# 
+# printN(1)
 
-printN(1)
+def fibo(n):
+   if n < 2:
+      return n
+   return fibo(n -1) + fibo(n -2)
+
+print(fibo(7))
