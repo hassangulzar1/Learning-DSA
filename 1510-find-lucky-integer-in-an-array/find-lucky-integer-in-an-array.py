226c079e054b39fbd70812c47e01df34be26637d
M.Hassan Gulzar <122821437+hassangulzar1@users.noreply.github.com>
2024-06-26 21:04:20 +0500

    Time: 57 ms (55.04%) | Memory: 16.6 MB (40.83%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -3,15 +3,12 @@ class Solution:
         hashMap = {}
 
         for i in arr:
-            if i not in hashMap:
-                hashMap[i] = 1
-            else:
-                hashMap[i] +=1
+            hashMap[i] = hashMap.get(i, 0) + 1
         
         highest = -1
 
         for key,val in hashMap.items():
             if key == val and key > highest:
                 highest = key
-                
+
         return highest
