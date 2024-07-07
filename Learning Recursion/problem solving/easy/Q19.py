6ebfdb329247e764dc366a38e57319901eef79d1
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-07-07 13:44:06 +0500

    q19 done

--- Learning Recursion/problem solving/easy/Q19.py
+++ Learning Recursion/problem solving/easy/Q19.py
@@ -1,14 +1,9 @@
 array = [20, 20, 78 ,98 ,99, 97]
 
 def arrayIsSorted(arr,n):
-    if n == 1:
+    if n == 1 or n == 0:
         return True
     
-    arrayIsSorted(arr,n-1)
-
-    if arr[n] <= arr[n + 1]:
-        return True
-    else:
-        return False
+    return arr[n - 1] <= arr[n] and arrayIsSorted(arr,n-1)
     
-print(arrayIsSorted(array,len(array)-2) )
+print(arrayIsSorted(array,len(array)-1) )
