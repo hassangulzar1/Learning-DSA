529a7ee4ef84f9b10dee4a18eea0cee829f634df
M.Hassan Gulzar <122821437+hassangulzar1@users.noreply.github.com>
2024-07-09 20:45:20 +0500

    Time: 37 ms (72.35%) | Memory: 16.7 MB (10.54%) - LeetSync

---
+++ 507-perfect-number/perfect-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+
+        if num <= 1:
+            return False
+        
+        sum_divesor = 1
+        sqrt_num = int(num ** 0.5)
+
+        for i in range(2,sqrt_num + 1):
+            if num % i == 0:
+                sum_divesor += i
+                if i != num // i:
+                    sum_divesor += num //  i
+        
+        return sum_divesor == num
+
+
+        
