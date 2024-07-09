83281902f69ccd3dbf06cc428395f9468b649feb
M.Hassan Gulzar <122821437+hassangulzar1@users.noreply.github.com>
2024-07-09 18:17:15 +0500

    Time: 31 ms (90.56%) | Memory: 16.7 MB (24.82%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        def helper(p,up):
+            if len(up) == 0:
+                return [p]
+
+            temp = [up[0]]
+
+            left = helper(p + temp, up[1:])
+            right = helper(p, up[1:])
+            return left + right
+        
+        return helper([],nums)
+        
