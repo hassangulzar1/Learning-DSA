5ee40ede72164d8520bd3c7a304d9676d1fa7576
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-07-14 17:34:38 +0500

    BST insertion done

--- java/Learning Trees/Trees 1/BST.py
+++ java/Learning Trees/Trees 1/BST.py
@@ -30,5 +30,44 @@ class BST:
             node.right = self._insert(value,node.right)
         
         node.height = max(self.height(node.left),self.height(node.right) + 1)
-        
+
         return node
+    
+    def populate(self,nums):
+        for i in range(len(nums)):
+            self.insert(nums[i])
+
+    
+    def balanced(self):
+        return self._balanced(self.root)
+    
+    def _balanced(self,node):
+        if node is None:
+            return True
+        
+        return abs(self.height(node.left) - self.height(node.right)) <= 1 and self._balanced(node.left) and self._balanced(node.right)
+    
+    def display(self):
+        self._display(self.root,"Root Node: ")
+
+    def _display(self,node,details):
+        if node is None:
+            return
+        
+        print(details + str(node.value))
+        self._display(node.left, "Left child of " + str(node.value) + " : ")
+        self._display(node.right, "Right child of " + str(node.value) + " : ")
+
+        
+
+tree = BST()
+nums = [5,2,7,1,4,6,9,8,3,10]
+tree.populate(nums)
+
+tree.display()
+
+    
+    
+
+
+
