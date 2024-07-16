d17da1c0812c267c236ddb3ac9a90b8a3b1ca9b7
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-07-16 17:29:00 +0500

    trees 5th question done

---
+++ java/Learning Trees/Tree Questions/Q5-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        from collections import deque
+
+        result = []
+
+        if root is None:
+            return result  
+
+        d = deque([root])
+
+        while d:
+            levelSize = len(d)
+            currLevel = []
+            for i in range(len(d)):
+                currNode = d.popleft()
+                currLevel.append(currNode.val)
+                if currNode.left:
+                    d.append(currNode.left)
+                
+                if currNode.right :
+                    d.append(currNode.right)
+                
+            result.insert(0,currLevel)
+
+        return result
+        
