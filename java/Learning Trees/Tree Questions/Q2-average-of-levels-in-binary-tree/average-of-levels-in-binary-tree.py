dfed903ddaea1a24aee4b5ce9b0bda63e28ff75e
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-07-16 14:24:42 +0500

    Q2 done

---
+++ java/Learning Trees/Tree Questions/Q2-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        averages = []
+
+        if root is None:
+            return averages
+        
+        queue = deque([root])
+
+        while queue:
+            levelSize = len(queue)
+            avg = 0
+
+
+            for i in range(levelSize):
+                currNode = queue.popleft()
+                avg += currNode.val
+
+                if currNode.left:
+                    queue.append(currNode.left)                               
+                                
+                if currNode.right:
+                    queue.append(currNode.right)
+            avg = (avg / levelSize)
+            
+            averages.append(avg)
+
+        return averages
+        
