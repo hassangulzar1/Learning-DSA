63243a610ccc83e29e6925f8b98a617c082f3caa
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-08-08 18:31:15 +0500

    sdf

--- java/10- heaps/Heap.py
+++ java/10- heaps/Heap.py
@@ -1,7 +1,6 @@
 class Heap:
     def __init__(self):
         self.list = []
-
     def swap(self, first, second):
         self.list[first], self.list[second] = self.list[second], self.list[first]
 
