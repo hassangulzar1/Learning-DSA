6b3828eaecd89ddc418ec9f5bd02ec27bd1e9f27
M.Hassan Gulzar <122821437+hassangulzar1@users.noreply.github.com>
2024-08-10 19:40:05 +0500

    Time: 45 ms (25.59%) | Memory: 16.6 MB (88.89%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def helper(self,i,nums):
+        if i == len(nums):
+            return [[]]
+        
+        res = []
+
+        perms = self.helper(i + 1,nums)
+
+        for p in perms:
+            for j in range(len(p) + 1):
+                pcopy = p.copy()
+                pcopy.insert(j,nums[i])
+                res.append(pcopy)
+                
+        return res
+
+
+
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+       return self.helper(0,nums)
