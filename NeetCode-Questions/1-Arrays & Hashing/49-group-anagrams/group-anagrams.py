f49eaa65217aa3ff4e6513e68c57673d0af7e10f
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-08-17 23:53:42 +0500

    done

--- NeetCode-Questions/1-Arrays & Hashing/49-group-anagrams/group-anagrams.py
+++ NeetCode-Questions/1-Arrays & Hashing/49-group-anagrams/group-anagrams.py
@@ -1,21 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        pairs = {}
+
+        for i in range(len(strs)):
+            sortedStr = "".join(sorted(strs[i]))
+            print(sortedStr)
+            if sortedStr not in pairs:
+                pairs[sortedStr] = []
+            
+            pairs[sortedStr].append(strs[i])
         ans = []
-        maping = {}
-
-        for string in strs:           
-            sorted_s = ''.join(sorted(string))
-
-            if sorted_s not in maping:
-                maping[sorted_s] = []
-
-            maping[sorted_s].append(string)
-        
-        for key, value in maping.items():
+        for key, value in pairs.items():
             ans.append(value)
-        
         return ans
-        
-        
 
         
