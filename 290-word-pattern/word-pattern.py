9be39b199a7fec436615d48b2c45e7efc5356234
M.Hassan Gulzar <122821437+hassangulzar1@users.noreply.github.com>
2024-08-30 17:30:12 +0500

    Time: 41 ms (14.19%) | Memory: 16.5 MB (37.45%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,16 +1,19 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        patMap = {}
-        sMap = {}
-   
         s = s.split(" ")
         if len(s) != len(pattern): return False
-        for ch in range(len(pattern)):
-            if (pattern[ch] in patMap and s[ch] != patMap[pattern[ch]] ) or (s[ch] in sMap and pattern[ch] != sMap[s[ch] ]):
-                return False
 
-            patMap[pattern[ch]] = s[ch]
-            sMap[s[ch]] = pattern[ch]
+        charMap = {}
+        wordMap = {}
+   
+
+        for c, w in zip(pattern,s):
+            if c in charMap and charMap[c] != w:
+                return False
+            if w in wordMap and wordMap[w] != c:
+                return False
+            charMap[c] = w
+            wordMap[w] = c
             
         return True
 
