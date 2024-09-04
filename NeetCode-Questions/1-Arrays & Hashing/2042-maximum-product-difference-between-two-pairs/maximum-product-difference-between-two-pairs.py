e7ea43c34ee8c7a06455ef41819488d958172e49
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-09-04 18:59:08 +0500

    O(n) solution

--- NeetCode-Questions/1-Arrays & Hashing/2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
+++ NeetCode-Questions/1-Arrays & Hashing/2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.py
@@ -1,5 +1,17 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        nums.sort()
+        max1,max2 = 0,0
+        min1,min2 = float("inf"),float("inf")
 
-        return (nums[len(nums) - 1] * nums[len(nums ) -2 ]) -  (nums[0] * nums[1])
+        for n in nums:
+            if n > max2:
+                if n > max1:
+                    max1,max2 = n, max1
+                else:
+                    max2 = n
+            if n < min2:
+                if n < min1:
+                    min1,min2 = n,min1
+                else:
+                    min2 = n
+        return (max1 * max2) - (min1 * min2)
