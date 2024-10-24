a297ec4277e9a97be4c17e7dd0f8664b2f1680fc
M.Hassan Gulzar <hassangulzar121@gmail.com>
2024-10-24 19:53:38 +0500

    done

--- NeetCode-Questions/6- linkedlist/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ NeetCode-Questions/6- linkedlist/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,6 +5,7 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
         dummy = ListNode(0,head)
 
         left = dummy
